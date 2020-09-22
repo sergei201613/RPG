b1a60f0489c2d15144bba02d0bb5c3286f0249a9
sergei201613 <sgorey@bk.ru>
2020-09-22 12:51:02 +0800

    make top down movement and create interactable component

---
+++ RPG/Assets/Prefabs/Player/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+    
+    private Camera _camera;
+    private PlayerMotor _motor;
+    private Interactable _focus;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out var hit, 100, movementMask))
+            {
+                _motor.MoveToPoint(hit.point);
+                RemoveFocus();
+            }
+        }
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out var hit, 100))
+            {
+                var interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                    SetFocus(interactable);
+            }
+        }
+    }
+
+    private void SetFocus(Interactable newFocus)
+    {
+        _focus = newFocus;
+        _motor.FollowTarget(newFocus);
+    }
+
+    private void RemoveFocus()
+    {
+        _focus = null;
+        _motor.StopFollowingTarget();
+    }
+}
---
+++ RPG/Assets/Prefabs/Player/PlayerMotor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Transform _target;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (_target != null)
+        {
+            _agent.SetDestination(_target.position);
+            FaceTarget();
+        }
+    }
+
+    public void MoveToPoint(Vector3 point)
+    {
+        _agent.SetDestination(point);
+    }
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        _agent.stoppingDistance = newTarget.radius * .8f;
+        _agent.updateRotation = false;
+        _target = newTarget.transform;
+    }
+
+    public void StopFollowingTarget()
+    {
+        _agent.stoppingDistance = 0f;
+        _agent.updateRotation = true;
+        _target = null;
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (_target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+}
